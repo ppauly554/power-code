2c57e2dffee949a4fdf07920361d1fbe53e6754f
ppauly554 <44756678+ppauly554@users.noreply.github.com>
2020-10-14 20:54:04 -0400

    Update Pascal's triangle.py

--- Pascal's triangle.py
+++ Pascal's triangle.py
@@ -1,1 +1,11 @@
-Print("yes")
+length = input("length?")
+triangle = [[0,1,0]]
+
+for chunk in range(length):
+    triangle.append([0])
+    for piece in range(1, len(triangle[chunk])):
+        triangle[chunk + 1].append(triangle[chunk][piece - 1] + triangle[chunk][piece])
+    triangle[chunk + 1].append(0)
+
+for part, space in enumerate(length - 1, -1, -1):
+    print(" "*space, triangle[part])
